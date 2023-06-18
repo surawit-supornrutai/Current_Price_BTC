68e6d9422e3a33b8ccf3fb329ccf1ab0ad5c0527
surawit supornrutai <105262445+surawit-supornrutai@users.noreply.github.com>
2023-06-18 15:48:40 +0700

    create project

---
+++ Current_Price_Btc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Current_Price_Btc
+//
+//  Created by Surawit Supornrutai on 18/6/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Current_Price_Btc' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Current_Price_Btc
+
+  pod 'Alamofire'
+  pod 'AlamofireImage'
+
+end
